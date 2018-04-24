786641ef0ceee5e790451495eea0d204a58090cc
Danny Milosavljevic <dannym@scratchpost.org>
2018-04-24 23:50:20 +0200

    Delete build_static_library.

--- build_static_library.mk
+++
@@ -1,8 +0,0 @@
-
-LOCAL_OBJ_FILES := $(patsubst %.c,%.o,$(LOCAL_SRC_FILES))
-
-$(LOCAL_MODULE).a: $(LOCAL_OBJ_FILES)
-	$(AR) rcs $@ $^
-
-%.o: %.c
-	$(CC) -c $(LOCAL_CFLAGS) -I $(LOCAL_C_INCLUDESX) $<
