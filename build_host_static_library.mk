ab14e260ce75d292e5a8713a59f657fb2cd72e48
Danny Milosavljevic <dannym@scratchpost.org>
2018-04-25 00:39:23 +0200

    Handle LOCAL_C_INCLUDES.

--- build_host_static_library.mk
+++ build_host_static_library.mk
@@ -1,8 +1,9 @@
 
 LOCAL_OBJ_FILES := $(patsubst %.c,%.o,$(LOCAL_SRC_FILES))
+LOCAL_C_INCLUDES_PARAMS := -I $(GOOGLETEST_INCLUDE) -I ../base/include -I ../include -I ../libsparse/include $(foreach d, $(LOCAL_C_INCLUDES), -I $d)
 
 $(LOCAL_MODULE).a: $(LOCAL_OBJ_FILES)
 	$(AR) rcs $@ $^
 
 %.o: %.c
-	$(CC) -c $(LOCAL_CFLAGS) -I $(LOCAL_C_INCLUDESX) $<
+	$(CC) -c $(LOCAL_CFLAGS) -I $(LOCAL_C_INCLUDES_PARAMS) $<
