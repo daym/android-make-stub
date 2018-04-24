08bf94caec19e7a43494668c604965d9f5fb13ab
Danny Milosavljevic <dannym@scratchpost.org>
2018-04-25 01:53:16 +0200

    Replace GOOGLETEST_INCLUDE by CFLAGS.

--- build_host_static_library.mk
+++ build_host_static_library.mk
@@ -1,9 +1,12 @@
 
-LOCAL_OBJ_FILES := $(patsubst %.c,%.o,$(LOCAL_SRC_FILES))
-LOCAL_C_INCLUDES_PARAMS := -I $(GOOGLETEST_INCLUDE) -I ../base/include -I ../include -I ../libsparse/include $(foreach d, $(LOCAL_C_INCLUDES), -I $d)
+LOCAL_OBJ_FILES := $(patsubst %.cpp,%.o,$(patsubst %.c,%.o,$(patsubst %.cc,%.o,$(LOCAL_SRC_FILES))))
+LOCAL_C_INCLUDES_PARAMS := -I ../base/include -I ../include -I ../libsparse/include $(foreach d, $(LOCAL_C_INCLUDES), -I $d)
 
 $(LOCAL_MODULE).a: $(LOCAL_OBJ_FILES)
 	$(AR) rcs $@ $^
 
 %.o: %.c
-	$(CC) -c $(LOCAL_CFLAGS) -I $(LOCAL_C_INCLUDES_PARAMS) $<
+	$(CC) -c $(CFLAGS) $(LOCAL_CFLAGS) -I $(LOCAL_C_INCLUDES_PARAMS) $<
+
+%.o: %.cc
+	$(CXX) -c $(CFLAGS) $(LOCAL_CFLAGS) -I $(LOCAL_C_INCLUDES_PARAMS) $<
