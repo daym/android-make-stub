daym/android-make-stub

commit a1e9e9c2fe451ada87680fe0bbc7c5632f4bebf0

Enable to build specific modules with LOCAL_MODULE=<local module>
Some Android.mk have multiple LOCAL_MODULEs, and by default,
android-make-stub builds all LOCAL_MODULEs.

And sometimes it is necessary to either change the order in which
LOCAL_MODULEs are built or to build only specific LOCAL_MODULEs.

And before this patch, passing LOCAL_MODULE=<local module> did not
enable to only build a specific local module. Instead it built only
the first LOCAL_MODULE defined in the Android.mk and produced a binary
whose name came from <local module>.

Now passing LOCAL_MODULE=<LOCAL_MODULE> to make enables to do that
(build only a specific local module).

Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

Makefile
@@ -23,3 +23,4 @@ install: all
 check:
 	make -C tests/single-host-executable-module
 	make -C tests/multiple-host-executable-modules
+	make -C tests/target-host-executable-module

tests/target-host-executable-module/Makefile
@@ -1,0 +1,12 @@
+# Copyright (C) 2022 Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
+#
+# This file is licensed under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+.PHONY: all
+all:
+	make -f ../../src/core/main.mk ; test $$? -eq 2
+	make -f ../../src/core/main.mk LOCAL_MODULE=hello
+	test "`./hello`" = "Hello World"

.gitignore
@@ -56,6 +56,7 @@ dkms.conf
 /tests/multiple-host-executable-modules/hello
 /tests/multiple-host-executable-modules/single-file
 /tests/single-host-executable-module/single-file
+/tests/target-host-executable-module/hello
 
 # Text editor temporary files
 *~

src/core/build_host_executable.mk
@@ -1,3 +1,15 @@
+# Copyright (C) 2018 Danny Milosavljevic <dannym@scratchpost.org>
+# Copyright (C) 2022 Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
+#
+# This file is licensed under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+include $(BUILD_SYSTEM)target_local_module.mk
+
+ifeq ($(LOCAL_MODULE),$(TARGET))
 
 LOCAL_C_OBJ_FILES := $(notdir $(filter %.o,$(patsubst %.c,%.o,$(LOCAL_SRC_FILES) $(LOCAL_SRC_FILES_linux))))
 LOCAL_CXX_OBJ_FILES := $(notdir $(filter %.o,$(patsubst %.cpp,%.o,$(patsubst %.cc,%.o,$(LOCAL_SRC_FILES) $(LOCAL_SRC_FILES_linux)))))
@@ -51,3 +63,5 @@ install-$(LOCAL_MODULE): LOCAL_MODULE := $(LOCAL_MODULE)
 install-$(LOCAL_MODULE): $(LOCAL_MODULE)
 	install -m 755 -d $(prefix)/bin
 	install -m 755 $(LOCAL_MODULE) $(prefix)/bin/$(LOCAL_MODULE)
+
+endif # ifneq ($(LOCAL_MODULE),$(TARGET))

src/core/build_host_native_test.mk
@@ -1,3 +1,15 @@
+# Copyright (C) 2018 Danny Milosavljevic <dannym@scratchpost.org>
+# Copyright (C) 2022 Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
+#
+# This file is licensed under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+include $(BUILD_SYSTEM)target_local_module.mk
+
+ifeq ($(LOCAL_MODULE),$(TARGET))
 
 # 1:1 copy of parts of build_host_executable.mk
 
@@ -49,3 +61,5 @@ check: LOCAL_MODULE := $(LOCAL_MODULE)
 check: LOCAL_MODULE_PARAMS := $($(subst -,_,$(LOCAL_MODULE))_PARAMS)
 check: $(LOCAL_MODULE)
 	./$(LOCAL_MODULE) $(LOCAL_MODULE_PARAMS)
+
+endif # ifneq ($(LOCAL_MODULE),$(TARGET))

src/core/build_host_shared_library.mk
@@ -1,3 +1,15 @@
+# Copyright (C) 2018 Danny Milosavljevic <dannym@scratchpost.org>
+# Copyright (C) 2022 Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
+#
+# This file is licensed under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+include $(BUILD_SYSTEM)target_local_module.mk
+
+ifeq ($(LOCAL_MODULE),$(TARGET))
 
 LOCAL_C_OBJ_FILES := $(notdir $(filter %.o,$(patsubst %.c,%.o,$(LOCAL_SRC_FILES))))
 LOCAL_CXX_OBJ_FILES := $(notdir $(filter %.o,$(patsubst %.cpp,%.o,$(patsubst %.cc,%.o,$(LOCAL_SRC_FILES) $(LOCAL_SRC_FILES_linux)))))
@@ -45,3 +57,5 @@ $(LOCAL_CXX_OBJ_FILES): CXXFLAGS := $(CXXFLAGS)
 $(LOCAL_CXX_OBJ_FILES): LOCAL_C_INCLUDES_PARAMS := $(LOCAL_C_INCLUDES_PARAMS)
 $(LOCAL_CXX_OBJ_FILES): $(LOCAL_SRC_FILES) $(LOCAL_SRC_FILES_linux)
 	$(CXX) -c -fPIC $(LOCAL_CFLAGS) $(CPPFLAGS) $(CXXFLAGS) $(LOCAL_C_INCLUDES_PARAMS) $^
+
+endif # ifneq ($(LOCAL_MODULE),$(TARGET))

src/core/build_host_static_library.mk
@@ -1,3 +1,15 @@
+# Copyright (C) 2018 Danny Milosavljevic <dannym@scratchpost.org>
+# Copyright (C) 2022 Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
+#
+# This file is licensed under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+include $(BUILD_SYSTEM)target_local_module.mk
+
+ifeq ($(LOCAL_MODULE),$(TARGET))
 
 LOCAL_C_OBJ_FILES := $(notdir $(filter %.o,$(patsubst %.c,%.o,$(LOCAL_SRC_FILES) $(LOCAL_SRC_FILES_linux))))
 LOCAL_CXX_OBJ_FILES := $(notdir $(filter %.o,$(patsubst %.cpp,%.o,$(patsubst %.cc,%.o,$(LOCAL_SRC_FILES) $(LOCAL_SRC_FILES_linux)))))
@@ -36,3 +48,5 @@ $(LOCAL_CXX_OBJ_FILES): CXXFLAGS := $(CXXFLAGS)
 $(LOCAL_CXX_OBJ_FILES): LOCAL_C_INCLUDES_PARAMS := $(LOCAL_C_INCLUDES_PARAMS)
 $(LOCAL_CXX_OBJ_FILES): $(LOCAL_SRC_FILES) $(LOCAL_SRC_FILES_linux)
 	$(CXX) -c -fPIC $(LOCAL_CFLAGS) $(CPPFLAGS) $(CXXFLAGS) $(LOCAL_C_INCLUDES_PARAMS) $^
+
+endif # ifneq ($(LOCAL_MODULE),$(TARGET))

src/core/main.mk
@@ -1,3 +1,19 @@
+# Copyright (C) 2018 Danny Milosavljevic <dannym@scratchpost.org>
+# Copyright (C) 2022 Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
+#
+# This file is licensed under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# We enable users to pass LOCAL_MODULE=<local module> to make, to only
+# build the specified local module. However we can't keep the value in
+# LOCAL_MODULE as it is then (re)defined in Android.mk
+
+TARGET := $(LOCAL_MODULE)
+undefine LOCAL_MODULE
+override undefine LOCAL_MODULE
 
 prefix = /usr
 HOST_OS = linux

commit 8e705132537266bad1bbcc85ef0f6f17b588f6fe

Makefile: install forgotten target_local_module.mk
Without that fix, when trying to build software with the commit
80466b806d634efb9a8d6e8bf3c5ffcffd75a4f8 ("fix
LOCAL_C_INCLUDES_PARAMS") of android-make-stub in Guix, we have the
following error:
    /gnu/store/[...]/share/android/build/core/build_host_shared_library.mk:10:
    /gnu/store/[...]/share/android/build/core/target_local_module.mk:
    No such file or directory make: *** No rule to make target
    '/gnu/store/[...]/share/android/build/core/target_local_module.mk'.
    Stop.

This issue was introduced in the commit
a1e9e9c2fe451ada87680fe0bbc7c5632f4bebf0 ("Enable to build specific
modules with LOCAL_MODULE=<local module>")

The test didn't detect this issue because all the tests that were done
for these commits were done with a local copy of android-make-stub
with either the built-in tests or with a modified library
(libsamsung-ipc) that had an Android.mk with multiple local modules.

Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

Makefile
@@ -20,6 +20,8 @@ install: all
 	install -m 644 src/core/build_host_static_library.mk $(prefix)/share/android/build/core/build_host_static_library.mk
 	install -m 644 src/core/clear_vars.mk $(prefix)/share/android/build/core/clear_vars.mk
 	install -m 644 src/core/main.mk $(prefix)/share/android/build/core/main.mk
+	install -m 644 src/core/target_local_module.mk $(prefix)/share/android/build/core/target_local_module.mk
+
 check:
 	make -C tests/single-host-executable-module
 	make -C tests/multiple-host-executable-modules